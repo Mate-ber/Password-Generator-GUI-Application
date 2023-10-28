d9f0b88c1bdb9df666b0e9ff3bdb16ff5e065611
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2023-10-29 01:23:43 +0700

    Add files via upload

---
+++ Password Generator GUI Application.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import random
+import string
+
+
+def generate_random_password():
+    length = len_password.get()
+    characters = string.ascii_letters + string.digits + string.punctuation
+    cur_password = ''.join(random.choice(characters) for _ in range(length))
+    final_password.set(cur_password)
+
+
+window = Tk()
+window.geometry("400x300")
+window.title("Random Password Generator")
+
+final_password = StringVar()
+len_password = IntVar()
+
+len_password.set(0)
+
+
+Label(window, text="Password Generator Application", font="calibri 20 bold").pack()
+Label(window, text="Enter password length").pack(pady=3)
+Entry(window, textvariable=len_password).pack(pady=3)
+Button(window, text="Click button to generate", command=generate_random_password).pack(pady=7)
+Label(window, text="Your Generated password").pack(pady=3)
+Entry(window, textvariable=final_password).pack(pady=3)
+
+
+window.mainloop()
